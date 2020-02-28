341e8b1b8e2d9fed665538ba22544331c2ef5527
Jared Remsberg <32494777+Gumbachi@users.noreply.github.com>
2020-02-28 15:21:08 -0500

    Update basic.py

--- cogs/basic.py
+++ cogs/basic.py
@@ -6,8 +6,7 @@ from vars import bot
 
 class BaseCommands(commands.Cog):
     """Handles all of the simple commands such as saying howdy or
-    the help command. These commands are unrelated to anything
-    color-related and most work in disabled channels
+    the help command.
     """
 
     def __init__(self, bot):
@@ -15,8 +14,7 @@ class BaseCommands(commands.Cog):
 
     @commands.command(name="help")
     async def help(self, ctx):
-        """The standard help command. Pulls info from vars.py and allows
-         users to select pages or specific commands"""
+        """The standard help command."""
         await ctx.send("Doesn't exist yet")
 
     @commands.command(name='howdy')
