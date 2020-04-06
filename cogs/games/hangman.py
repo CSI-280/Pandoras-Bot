5fe905d17c4436f6e1ba2edcb667012211e15a42
Zachary Jones <zachary.jones@mymail.champlain.edu>
2020-04-06 15:37:38 -0400

    Worked on getting hangman to connect to the bot and added onto the hangman.py for more functionality

--- cogs/games/hangman.py
+++ cogs/games/hangman.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import UserInputError
+
+from vars import bot, get_prefix, get_help
+from auth import authorize
+
 import random
 import json
 
@@ -11,27 +16,52 @@ name = "calvin"
 
 print("User " + name + " has started a game of hangman.")
 
-# Max amount of times someone can guess incorrectly
-turns = 10
-guesses = ''
-word = random.choice(word_list)
 
-while turns > 0:
-    print("Guesses left: " + str(turns))
-    failed = 0
-    for char in word:
-        if char in guesses:
-            print(char)
-        else:
-            print("_")
-            failed += 1
-    if failed == 0:
-        print("User " + name + " has won hangman.")
-        break
-    guess = input("Guess a character: ")
-    guesses += guess
-    if guess not in word:
-        turns -= 1
-        print("That character is not in the word")
-    if turns == 0:
-        print("User " + name + " has lost a game of hangman.")
+class Game:
+    """Class for playing the game"""
+    # Max amount of times someone can guess incorrectly
+    turns = 10
+    guesses = ''
+    word = random.choice(word_list)
+
+    while turns > 0:
+        print("Guesses left: " + str(turns))
+        failed = 0
+        for char in word:
+            if char in guesses:
+                print(char)
+            else:
+                print("_")
+                failed += 1
+        if failed == 0:
+            print("User " + name + " has won hangman.")
+            break
+        guess = input("Guess a character: ")
+        guesses += guess
+        if guess not in word:
+            turns -= 1
+            print("That character is not in the word")
+        if turns == 0:
+            print("User " + name + " has lost a game of hangman.")
+
+
+class Hangman(commands.Cog):
+    """Handles the commands associated with the game"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="hangman", aliases=["h", "hm"])
+    async def hangman(self, ctx):
+
+        """Starts up a game of hangman"""
+        authorize(ctx, "mentions")  # check for a mentioned user
+
+        p1 = ctx.author
+        p2 = ctx.message.mentions[0]  # first mentioned user
+
+        if p1 == p2:
+            raise UserInputError("Can't play against yourself")
+
+def setup(bot):
+    bot.add_cog(Hangman(bot))
