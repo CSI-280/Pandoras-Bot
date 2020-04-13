023de5e6c12a5ded8b25e9026b50c76df0ec5c49
calrose <48226669+calrose@users.noreply.github.com>
2020-04-13 19:09:55 -0400

    Update battleship.py

--- cogs/games/battleship.py
+++ cogs/games/battleship.py
@@ -52,13 +52,81 @@ class BattleShip:
 
     def place_ships(self, board, ships):
         # Place the user's ships and validate location.
+        for ship in ships.keys():
+
+            # get coordinates from user and validate the postion
+            valid = False
+            while (not valid):
+
+                self.print_board("u", board)
+                print("Placing a/an ") + ship
+                x, y = self.get_coor()
+                ori = self.v_or_h()
+                valid = self.validate(board, ships[ship], x, y, ori)
+                if not valid:
+                    print("Cannot place a ship there.\nPlease take a look at the board and try again.")
+                    input("Hit ENTER to continue")
+
+            # place the ship
+            board = self.place_ship(board, ships[ship], ship[0], ori, x, y)
+            self.print_board("u", board)
+
+        input("Done placing user ships. Hit ENTER to continue")
+        return board
+
+    def place_ships_user_2(self, board, ships):
+        # Place the second user's ships and validate location.
+        for ship in ships.keys():
+
+            # get coordinates from user and validate the postion
+            valid = False
+            while (not valid):
+
+                self.print_board("s", board)
+                print("Placing a/an ") + ship
+                x, y = self.get_coor()
+                ori = self.v_or_h()
+                valid = self.validate(board, ships[ship], x, y, ori)
+                if not valid:
+                    print("Cannot place a ship there.\nPlease take a look at the board and try again.")
+                    input("Hit ENTER to continue")
+
+            # place the ship
+            board = self.place_ship(board, ships[ship], ship[0], ori, x, y)
+            self.print_board("u", board)
+
+        input("Done placing user ships. Hit ENTER to continue")
+        return board
 
     def place_ship(self, board, ship, s, ori, x, y):
         # Place the ship at the correct orientation(vertical or horizontal)
+        if ori == "v":
+            for i in range(ship):
+                board[x + i][y] = s
+        elif ori == "h":
+            for i in range(ship):
+                board[x][y + i] = s
+
+        return board
 
     def validate(self, board, ship, x, y, ori):
         # Validate that the ship can be placed at given coordinates.
+        if ori == "v" and x + ship > 10:
+            return False
+        elif ori == "h" and y + ship > 10:
+            return False
+        else:
+            if ori == "v":
+                for i in range(ship):
+                    if board[x + i][y] != -1:
+                        return False
+            elif ori == "h":
+                for i in range(ship):
+                    if board[x][y + i] != -1:
+                        return False
 
+        return True
+    
     def v_or_h(self):
         # While true, ask the user if they want their ship placed vertically or horizontally.
 
