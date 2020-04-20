cb2ac2e4f505d40c01389806621e7187ca161643
Zachary Jones <zachary.jones@mymail.champlain.edu>
2020-04-20 10:53:42 -0400

    Updated the get_coor() and v_or_h functions

--- cogs/games/battleship.py
+++ cogs/games/battleship.py
@@ -130,19 +130,126 @@ class BattleShip:
     def v_or_h(self):
         # While true, ask the user if they want their ship placed vertically or horizontally.
 
+        user_oritentation = ""
+        invalid_input = True
+
+        while invalid_input is True:
+
+            # Prompt the user for an input
+            user_input = input(
+                "Enter either h or v for your ships direction (horizontal or vertical): ")
+
+            # Vertical Ship Placement
+            if user_input == "v":
+                print("\nShip will be placed vertically")
+                invalid_input = False
+
+            # Horizontal ship placement
+            elif user_input == "h":
+                print("\nShip will be placed horizontally")
+                invalid_input = False
+
+            # User Input was neither horizontal nor vertical
+            else:
+                print("Input was invalid, please try again (enter h or v)\n")
+                invalid_input = True
+
+        # If the input was either h or v return ship orientation
+        if not invalid_input:
+            user_oritentation = user_input
+
+        return user_oritentation
+
+
     def get_coor(self):
         # While true, ask the user for the coordinates they wish to attack at.
 
+        user_coordinates = ()
+        confirmation = ""
+
+        # While loop to check if the coordinates are what the user actually wants
+        while confirmation != "y":
+
+            # While loop to check if the x coordinate is valid
+            while True:
+                # Check the x coordinate
+                try:
+                    # See if it's an integer
+                    x = int(input(
+                        "Enter the x coordinate you'd like to place a ship or attack at (between 0-9): "))
+                except ValueError:
+                    print("ERROR: Input is not a number")
+                else:
+                    # See if it's between 0-9, if it is move on to the y coord.
+                    if 0 <= x <= 9:
+                        break
+                    else:
+                        print("ERROR: Number is not in range, try again")
+                        continue
+            # While loop to check if the y coordinate is valid
+            while True:
+
+                # Check the y coordinate
+                try:
+                    # Check if it's an integer
+                    y = int(input(
+                        "Enter the y coordinate you'd like to place a ship or attack at (between 0-9): "))
+                except ValueError:
+                    print("ERROR: Input is not a number")
+                else:
+                    # See if it's between 0-9, if it is confirm this is the coord. the user wants
+                    if 0 <= y <= 9:
+                        break
+                    else:
+                        print("ERROR: Number is not in range, try again")
+                        continue
+
+            print(f"You chose coordinates {(x, y)} is this the correct position?")
+
+            confirmation = input("\nEnter either y or n: ")
+
+            # If yes, then it is the coord. that the player desires
+            if confirmation == "y":
+                user_coordinates = (x, y)
+                print(f"\nConfirmed coordinated for {user_coordinates}")
+                break
+
+            # If no, then the player didn't put the correct coord. in, and should try again
+            elif confirmation == "n":
+                print("Incorrect coordinates, try again\n")
+                continue
+
+            # If the input is neither yes or no, try again until valid input
+            while confirmation != "y" or confirmation != "n":
+                print("This is not a valid option, try again")
+                confirmation = input("Enter y or n: ")
+
+                if confirmation == "y":
+                    user_coordinates = (x, y)
+                    print(f"\nConfirmed coordinated for {user_coordinates}")
+                    break
+
+                elif confirmation == "n":
+                    print("Incorrect coordinates, try again\n")
+                    break
+
+        # Return the confirmed coordinates
+        return user_coordinates
+
     def make_move(self, board,x,y):
         # Make a move on the board and return the result, hit, miss or try again for repeat hit.
+        print("")
 
     def user_move(self, board):
         # Get coordinates from the user and try to make move.
         # If move is a hit, check ship sunk and win condition.
+        print("")
 
     def check_sink(self, board, x, y):
         # Check which kind of ship is hit, mark the coordinate as a hit, then check if ship is sunk.
+        print("")
 
     def check_win(self, board):
         # Check the board to see if win condition is met. If there's a char that isn't hit or miss, return false.
+        print("")
 
